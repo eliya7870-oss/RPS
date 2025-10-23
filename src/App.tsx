83167aa71b9a9b4666d498b5e1b5c30fd34e4c2e
eliya7870-oss <eliya7870@gmail.com>
2025-10-23 11:02:01 +0300

    basic rps with buttons and game state

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pcMove,setPcMove]=useState<null |string>(null)
+  const [res,setRes]=useState<null|string>(null)
+  const [games,setGames] = useState({win:0,lose:0,tie:0})
 
+  function pcMakeMove(){
+    let rand = Math.random()
+    let move = ''
+    if (rand>=0&&rand<1/3){
+      move = 'rock'
+    }
+    else if (rand>=1/3&&rand<2/3){
+      move = 'paper'
+    }
+    else{
+      move ='scissors'
+    }
+    setPcMove(move)
+    return(move)
+  }
+function play(playerMove:string){
+  let move = pcMakeMove()
+  if (playerMove ==='rock'){
+    switch(move){
+      case 'rock':
+        setRes('tie')
+        setGames(prev => ({ ...prev, tie: prev.tie++ }))
+        break;
+      case 'paper':
+        setRes('lose')
+        setGames(prev => ({ ...prev, lose: prev.lose++ }))
+        break;
+      case 'scissors':
+        setRes('win')
+        setGames(prev => ({ ...prev, win: prev.win++ }))
+        break;
+      default:
+        console.log('invalid move')
+    }
+  }
+  else if(playerMove==='paper'){
+    switch(move){
+      case 'rock':
+        setRes('win')
+        setGames(prev => ({ ...prev, win: prev.win++ }))
+        break;
+      case 'paper':
+        setRes('tie')
+        setGames(prev => ({ ...prev, tie: prev.tie++ }))
+        break;
+      case 'scissors':
+        setRes('lose')
+        setGames(prev => ({ ...prev, lose: prev.lose++ }))
+        break;
+      default:
+        console.log('invalid move')
+    }
+  }
+  else if(playerMove==='scissors'){
+    switch(move){
+      case 'rock':
+        setRes('lose')
+        setGames(prev => ({ ...prev, lose: prev.lose++ }))
+        break;
+      case 'paper':
+        setRes('win')
+        setGames(prev => ({ ...prev, win: prev.win++ }))
+        break;
+      case 'scissors':
+        setRes('tie')
+        setGames(prev => ({ ...prev, tie: prev.tie++ }))
+        break;
+      default:
+        console.log('invalid move')
+    }
+  }
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button onClick={()=>{play('rock')}}>rock</button>
+    <button onClick={()=>{play('paper')}}>paper</button>
+    <button onClick={()=>{play('scissors')}}>scissors</button>
+    {res&&<h1>computer chose {pcMove}, you {res}!</h1>}
+    <h1>lose: {games.lose},win: {games.win},tie: {games.tie}</h1>
+      
     </>
   )
 }
